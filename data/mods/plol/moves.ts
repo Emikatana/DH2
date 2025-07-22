89aae2e35a8ce5dbfa44a03c946d9ce0540ab2ed
Emikatana <itsemi48@gmail.com>
2025-07-22 13:40:38 +0200

    adding basic attack

--- data/mods/plol/moves.ts
+++ data/mods/plol/moves.ts
@@ -1,1 +1,26 @@
-
+export const Moves: { [moveid: string]: ModdedMoveData } = {
+	basicattack: {
+		num: -001,
+		accuracy: 100,
+		basePower: 80,
+		category: "Special",
+		name: "Basic Attack",
+		pp: 15,
+		priority: 0,
+		flags: { protect: 1, mirror: 1, metronome: 1 },
+		onModifyType(move, pokemon) {
+			const types = pokemon.getTypes();
+			let type = types[0];
+			move.type = type;
+		},
+		onModifyMove(move, pokemon) {
+			if (pokemon.getStat('atk', false, true) > pokemon.getStat('spa', false, true)) {
+				move.category = 'Physical';
+			}
+		},
+		secondary: null,
+		target: "normal",
+		type: "Normal",
+		contestType: "Beautiful",
+	},
+};
