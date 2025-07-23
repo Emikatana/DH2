8b95a19c9966062de8087fa4c51c8c704800df37
Emikatana <itsemi48@gmail.com>
2025-07-23 09:16:27 +0200

    deathbringer stance

--- data/mods/plol/abilities.ts
+++ data/mods/plol/abilities.ts
@@ -1,1 +1,25 @@
-
+export const Abilities: { [abilityid: string]: ModdedAbilityData } = {
+	
+	deathbringerstance: {
+		onBasePowerPriority: 30,
+		onBasePower(basePower, attacker, defender, move) {
+			if (move.id === 'basicattack' && !attacker.getVolatile('deathbringerstance')) {
+				move.basePower = Math.max(move.basePower + 20, 0);
+			}
+		},
+		onSourceDamagingHit(damage, target, source, move) {
+			if (move.id === 'basicattack' && !source.getVolatile('deathbringerstance')) {
+				this.heal(move.drain[1], source, target, 'drain');
+				source.addVolatile('deathbringerstance')
+			}
+		},
+		condition: {},
+		flags: {},
+		name: "Deathbringer Stance",
+		rating: 5,
+		num: -001,
+	},
+	
+	
+	
+};
