b637c8d81258afca7f1afde25e3daa161d7d7908
Emikatana <itsemi48@gmail.com>
2025-07-24 08:24:36 +0200

    formats data for plol

--- data/mods/plol/formats-data.ts
+++ data/mods/plol/formats-data.ts
@@ -1,1 +1,19 @@
+export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 
+	aatrox: {tier: "PLOL"},
+	ahri: {tier: "PLOL"},
+	akshan: {tier: "PLOL"},
+	alistar: {tier: "PLOL"},
+	amumu: {tier: "PLOL"},
+	anivia: {tier: "PLOL"},
+	annie: {tier: "PLOL"},
+	ashe: {tier: "PLOL"},
+	bard: {tier: "PLOL"},
+	brand: {tier: "PLOL"},
+	braum: {tier: "PLOL"},
+	briar: {tier: "PLOL"},
+	caitlyn: {tier: "PLOL"},
+	cassiopeia: {tier: "PLOL"},
+	chogath: {tier: "PLOL"},
+	
+};
