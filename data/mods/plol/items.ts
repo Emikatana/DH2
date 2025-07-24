005355705b2835230d7b2dd3454c7be1d05f0cef
Emikatana <itsemi48@gmail.com>
2025-07-24 08:47:01 +0200

    aatrox

--- data/mods/plol/items.ts
+++ data/mods/plol/items.ts
@@ -1,1 +1,11 @@
-
+export const Items: {[itemid: string]: ModdedItemData} = {
+	aatroxultimate: {
+		name: "Aatrox Ultimate",
+		spritenum: 646,
+		onTakeItem: false,
+		zMove: "World Ender",
+		zMoveFrom: "Basic Attack",
+		itemUser: ["Aatrox"],
+		num: -0001,
+		gen: 9,
+};
