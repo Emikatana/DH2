ba271ebdf1b41cd6cedc578cc0d59a42bc39c474
Emikatana <itsemi48@gmail.com>
2025-07-24 08:41:36 +0200

    aatrox

--- data/mods/plol/pokedex.ts
+++ data/mods/plol/pokedex.ts
@@ -1,1 +1,10 @@
-
+export const Pokedex: { [k: string]: ModdedSpeciesData } = {
+	aatrox: {
+		num: -1000,
+		name: "Aatrox",
+		types: ["Fire", "Dark"],
+		baseStats: { hp: 114, atk: 130, def: 76, spa: 50, spd: 58, spe: 62 },
+		abilities: { 0: "Deathbringer Stance" },
+		weightkg: 30,
+	},
+};	
