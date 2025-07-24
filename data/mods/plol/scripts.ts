eee8660f301bd81c87cd15e7b83dbe2863e25185
Emikatana <itsemi48@gmail.com>
2025-07-24 08:44:08 +0200

    aatrox

--- data/mods/plol/scripts.ts
+++ data/mods/plol/scripts.ts
@@ -1,1 +1,15 @@
+export const Scripts: ModdedBattleScriptsData = {
+	gen: 9,
+	teambuilderConfig: {
+		excludeStandardTiers: true,
+		customTiers: ["PLOL"],
+	},
 
+	init() {
+		this.modData('Learnsets', 'aatrox').learnset.basicattack = ['9L1'];
+		this.modData('Learnsets', 'aatrox').learnset.thedarkinblade = ['9L1'];
+		this.modData('Learnsets', 'aatrox').learnset.infernalchains = ['9L1'];
+		this.modData('Learnsets', 'aatrox').learnset.umbraldash = ['9L1'];
+		this.modData('Learnsets', 'aatrox').learnset.worldender = ['9L1'];
+	},
+};
