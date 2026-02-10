6aa41a6ad65aeb6525359d25bbbea313aba7b225
Emikatana <itsemi48@gmail.com>
2026-02-10 08:54:16 -0500

    formats data

---
+++ data/mods/rift/formats-data.ts
@@ -0,0 +1,50 @@
+export const FormatsData: import('../../../sim/dex-species').ModdedSpeciesFormatsDataTable = {
+    aatrox: {
+		tier: "RIFT",
+	},
+    ahri: {
+		tier: "RIFT",
+	},
+    akshan: {
+		tier: "RIFT",
+	},
+    alistar: {
+		tier: "RIFT",
+	},
+    amumu: {
+		tier: "RIFT",
+	},
+    anivia: {
+		tier: "RIFT",
+	},
+    annie: {
+		tier: "RIFT",
+	},
+    ashe: {
+		tier: "RIFT",
+	},
+    bard: {
+		tier: "RIFT",
+	},
+    brand: {
+		tier: "RIFT",
+	},
+    braum: {
+		tier: "RIFT",
+	},
+    briar: {
+		tier: "RIFT",
+	},
+    caitlyn: {
+		tier: "RIFT",
+	},
+    cassiopeia: {
+		tier: "RIFT",
+	},
+    chogath: {
+		tier: "RIFT",
+	},
+    twitch: {
+		tier: "RIFT",
+	},
+};
